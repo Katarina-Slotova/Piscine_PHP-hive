030b84e0cecb1e3051fa0a6b737a864bce348592
Katarina-Slotova <katarina.slotova@gmail.com>
2022-04-13 02:36:03 +0300

    Modified ex02 and ex03.

--- d02/ex02/magnifying_glass.php
+++ d02/ex02/magnifying_glass.php
@@ -4,15 +4,15 @@
 	if ($argc == 2 && file_exists($argv[1]))
 	{
 		$content = file_get_contents($argv[1]);
-		$pattern1 = "/<a>[\s](.*?)<\/a>/";
-		preg_match($pattern1, $content, $matches);
-		$str = implode(" ", $matches);
-		$pattern2 = "/<title>(.*?)<\/title>/";
-		preg_match($pattern2, $str, $matches);
-		$new_content = $matches;
+		$pattern1 = "/(<a )(.*?)(>)(.*)(<\/a>)/";
+		preg_match_all($pattern1, $content, $matches);
+		//$str = implode(" ", $matches[0]);
+		$pattern2 = "/(title=\")(.*?)(\")(>)(.*?)(<)/";
+		preg_match_all($pattern2, $content, $matches);
+		$new_content = $matches[0];
 		foreach ($new_content as &$data)
 			$data = strtoupper($data);
-		$content = preg_replace($matches, $new_content, $content);
+		$content = preg_replace($matches[0], $new_content, $content);
 		echo($content);
 	}
 
--- d02/ex03/who.php
+++ d02/ex03/who.php
@@ -9,14 +9,18 @@ function ft_remove_space($array)
 		return ;
 }
 
+$content = fopen("/var/run/utmpx", "r");
+$data = fread($content, filesize("/var/run/utmpx"));
 $people = array();
+$people = explode(" ", $people);
 
 foreach ($people as $name)
 {
-	date_default_timezone_set('Europe/Helsinki');
-	$date = date('d-m-y h:i:s');
+/* 	date_default_timezone_set('Europe/Helsinki');
+	$date = date('d-m-y h:i:s'); */
 	$data = explode(" ", $data[0]);
 	$data = array_filter($data, "ft_remove_space");
+	$data = array_values($data);
 	echo($data);
 }
 
