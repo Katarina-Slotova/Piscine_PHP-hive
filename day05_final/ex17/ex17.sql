caa5b74c045da0101dd0783382a45b8223dc46d6
Katarina Slotova <kslotova@c1r6p14.hive.fi>
2022-04-18 15:56:39 +0300

    Modified day05.

---
+++ day05_final/ex17/ex17.sql
@@ -0,0 +1,7 @@
+SELECT COUNT(id_sub) AS 'nb_susc', ROUND(AVG(price)) AS 'av_susc', MOD(SUM(duration_sub), 42) AS 'ft'
+FROM subscription;
+
+
+Display the number of offered subscription in a column named nb_susc,
+as well as the average subscription price, rounded (down) to the nearest unit in a column named av_susc. There must be a third column named
+ft displaying the sum of the subscription durations, modulo 42.
