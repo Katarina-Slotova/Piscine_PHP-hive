0a95604889daee5169fee05f9af069338a949aaf
Katarina Slotova <kslotova@c1r6p16.hive.fi>
2022-04-19 18:47:57 +0300

    Add ex00-ex21, all working.

--- day05_final/ex18/ex18.sql
+++ day05_final/ex18/ex18.sql
@@ -2,7 +2,3 @@ SELECT `name`
 FROM distrib
 WHERE id_distrib = 42 OR id_distrib BETWEEN 62 AND 69 OR id_distrib = 71 OR id_distrib BETWEEN 88 AND 90 OR `name` REGEXP '^[^y]*y[^y]*y[^y]*$'
 LIMIT 2, 5;
-/* LIMIT’s optional OFFSET argument is placed before the argument indicating the 
-number of returned rows. It indicates the position of the first row returned by LIMIT 
-(i.e. ‘0’ is the first row, 1 is the second row, etc.). */
-/* `name` LIKE '%y%y%' AND name NOT LIKE '%y%y%y%' */
